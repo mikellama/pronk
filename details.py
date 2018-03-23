fc673b2be301815ce2994c54f220e07314adae73
mikellama <36346265+mikellama@users.noreply.github.com>
2018-03-23 00:01:39 -0400

    Update details.py

--- details.py
+++ details.py
@@ -1,5 +1,5 @@
-nick = "mwaaa"             # the nickname the bot uses
-username = "mwaaa"         # the username the bot uses
-channel = "##bot-test"       # the channel  the bot joins, when you start the bot script 
+nick = "pronk"             # the nickname the bot uses
+username = "pronk"         # the username the bot uses
+channel = "##llamas"       # the channel  the bot joins, when you start the bot script 
 secret = "notreallythis"   # -
 admins = ["mikez"]         # the list of Admins for the bot, who can give commands 
