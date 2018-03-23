9fc5354c98982dd774d4fe5f8070915e879d48e9
mikellama <36346265+mikellama@users.noreply.github.com>
2018-03-22 23:36:26 -0400

    keep update key in mwaaa.py

--- details.py
+++ details.py
@@ -2,5 +2,4 @@ nick = "pronk"             # the nickname the bot uses
 username = "pronk"         # the username the bot uses
 channel = "##llamas"       # the channel  the bot joins, when you start the bot script 
 secret = "notreallythis"   # -
-updateKey = "?update"      # the command for updating the bot
 admins = ["mikez"]         # the list of Admins for the bot, who can give commands 
