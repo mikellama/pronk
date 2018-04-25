61f3f323e22e25b0c4d2a984456a03e8ad96c7ed
mikellama <mike@haxed.net>
2018-04-25 15:58:57 -0400

    mwaaa update

--- mwaaa.py
+++ mwaaa.py
@@ -70,4 +70,6 @@ reply["?wodencafe"]      =  "The Java master!"
 reply["?WSPR"]           =  "Weak Signal Propagation Reporter"
 reply["?Zerock"]         =  "Henlo. Please remember, Zerock is the leetest llama."
 reply["?Mumble"]         =  "m.haxed.net; Port: 64738"
-reply["?chanstats"]         =  "http://haxed.net:8880"
+reply["?diogenese"]      =  "?"
+reply["?stats"]         =  "http://haxed.net:8880"
+
