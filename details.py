b4d67fddc4b939c6183a339431ade199e26bf88a
DetectiveTaco <woodchristian374@gmail.com>
2018-05-29 11:49:14 -0400

    Wordnik api line and nickserv desc
    Add wordnik api key line and added a desc for the nickserv password line

--- details.py
+++ details.py
@@ -1,5 +1,6 @@
 nick = "pronk"             # the nickname the bot uses
 username = "pronk"         # the username the bot uses
 channel = "##llamas"       # the channel  the bot joins, when you start the bot script 
-secret = "notreallythis"   # -
+secret = "notreallythis"   # The NickServ password for your bot. 
 admins = ["mikez"]         # the list of Admins for the bot, who can give commands 
+wnAPI_key = "here"         # Wordnik API key. Required for the ?dict function.
