bfc69355d27a0661ea06f21af7acb56994a66f7c
DetectiveTaco <woodchristian374@gmail.com>
2018-05-29 12:49:49 -0400

    Added weather underground api line
    added weather underground API key line

--- details.py
+++ details.py
@@ -3,4 +3,5 @@ username = "pronk"         # the username the bot uses
 channel = "##llamas"       # the channel  the bot joins, when you start the bot script 
 secret = "notreallythis"   # The NickServ password for your bot. 
 admins = ["mikez"]         # the list of Admins for the bot, who can give commands 
-wnAPI_key = "here"         # Wordnik API key. Required for the ?dict function.
+wnAPI_key = "here"         # Your Wordnik API key goes here. Required for the ?dict function.
+wuAPI_key = "here"         # Your WeatherUnderground API key goes here. Required for the ?weather function.
