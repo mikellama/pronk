e0c83325b27a7eadbed8b3eddfa16e139af5811d
mikellama <mike@haxed.net>
2018-10-17 18:18:48 -0400

    stream notifier

--- llamabot.py
+++ llamabot.py
@@ -44,7 +44,10 @@ import re, socket, os, time, ssl, base64
 import actions
 import mwaaa
 import details
-
+import requests
+import json     
+import threading
+import time
 
 ## open socket
 sock = socket.socket()
@@ -82,6 +85,66 @@ msgMem = []
 last_ping = time.time()
 timeout = 15 * 60
 
+## Stream Checker
+class Checker(threading.Thread):
+    def __init__(self):
+        super(Checker, self).__init__()
+        self.daemon = True
+        self.paused = True
+        self.state = threading.Condition()
+        self.delay = 15
+        self.notifyTime = time.time()
+        self.streaming = False
+
+    def run(self):
+        while True:
+            with self.state:
+                if self.paused:
+                    self.state.wait()
+            try:
+                q = requests.get("http://letty.tk:8000/status-json.xsl")
+                js = json.loads(q.text)
+                if "source" in js["icestats"]:
+                    # check if it just came up or an annoncement was made recently
+                    if self.streaming == False and time.time() - self.notifyTime > 1 * 60:
+                        print("streaming!")
+                        sock.send("PRIVMSG "+details.channel+" :Music is streaming at http://letty.tk:8000/stream.mp3 !!\n")
+                        self.notifyTime = time.time()
+                    self.streaming = True
+                else:
+                    if self.streaming == True and time.time() - self.notifyTime > 5* 60:
+                        print("not streaming... but server is online")
+                        sock.send("PRIVMSG "+details.channel+" :Silence.\n")
+                        self.notifyTime = time.time()
+                    self.streaming = False
+            except:
+                if self.streaming == True and time.time() - self.notifyTime > 5 * 60:
+                    print("can't find server")
+                    sock.send("PRIVMSG "+details.channel+" :It's quiet now.\n")
+                    self.notifyTime = time.time()
+                self.streaming = False
+            time.sleep(self.delay)
+
+    def resume(self):
+        with self.state:
+            self.paused = False
+            self.state.notify()
+
+    def pause(self):
+        with self.state:
+            self.paused = True
+
+    def setDelay(self, d):
+        self.delay = d
+    
+    def noMsg(self):
+        self.streaming = True
+
+
+checker = Checker()
+checker.start()
+checker.pause()
+
 while True:
     msg = sock.recv(2048)
     msg = msg.strip("\n\r")  
@@ -114,8 +177,8 @@ while True:
         reload(mwaaa)
         reload(details)
 
+    msgLow = msg.lower()
     for command in actions.commands:
-        msgLow = msg.lower()
         if msgLow.find(command.lower()) != -1:
             r = actions.act(command, msg, sender, msgMem)
             if len(r) > 0:
@@ -127,6 +190,21 @@ while True:
                         sock.send("PRIVMSG "+sender+" :"+r+"\n")
                 except IndexError:
                     pass
+
+
+    ## special commands for the stream notifier
+    if msg.find("?notify-off") != -1 and sender in details.admins:
+        checker.pause()
+    elif msg.find("?notify-on") != -1 and sender in details.admins:
+        checker.resume()
+    elif msg.find("?noMsg") != -1 and sender in details.admins:
+        checker.noMsg()
+    elif msg.find("?delay") != -1 and sender in details.admins:
+        try:
+            d = int(msg[msg.lower().find("?delay") + 6:])
+            checker.setDelay(d)
+        except:
+            pass
 
 print("It's all over man.")
 
